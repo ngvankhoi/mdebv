50a3884f7fdd4191be60a0e842608bb0be398fba
Nguyễn Văn Khôi <khoi.nguyen@toancausoft.com.vn>
2014-05-21 16:23:03 -0700

    Signed-off-by: Nguyễn Văn Khôi <khoi.nguyen@toancausoft.com.vn>

--- Medisoft/AssemblyInfo.cs
+++ Medisoft/AssemblyInfo.cs
@@ -28,8 +28,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("20.05.14.1")]
-[assembly: AssemblyFileVersion("20.05.14.1")]
+[assembly: AssemblyVersion("21.05.14.1")]
+[assembly: AssemblyFileVersion("21.05.14.1")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
